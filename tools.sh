add213c072ce450179ac1c1add64a729792bfd9b
kyuusaku <q.suo@foxmail.com>
2014-10-09 22:00:13 +0800

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,19 @@
+yum install git -y
+yum install htop -y
+yum install byobu -y
+
+yum install gcc -y #gcc4.4.7
+yum install gcc-c++ -y
+yum install gcc-gfortran -y
+#yum install cmake -y #cmake2.6.4 (need >=2.8.8)
+
+yum install pcre-devel -y  #for swig
+
+yum install atlas -y
+yum install atlas-devel -y
+yum install atlas-sse2 -y
+yum install atlas-sse2-devel -y 
+yum install lapack-devel -y 
+
+yum install zlib-devel -y #for python pip
+yum install openssl-devel -y #for python pip
